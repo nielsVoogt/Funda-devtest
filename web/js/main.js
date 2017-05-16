7f0c885590ab3b3513be29e4ed08b05565539c29
Niels Voogt <niels@branchonline.nl>
2017-05-16 13:19:20 +0200

    Cleaning up and final touches + added some comments

--- web/js/main.js
+++ web/js/main.js
@@ -3,6 +3,19 @@
 
 $('._funda-tile-show-more').on('click', function(e) {
     e.preventDefault();
-    $(this).toggleClass('triggered');
-    $(this).closest( ".funda-tile" ).toggleClass('funda-tile--triggered');
+    var $this = $(this);
+    $this.toggleClass('triggered');
+    $this.closest('.funda-tile').toggleClass('funda-tile--show-additional-content');
+    if($this.hasClass('triggered')){
+        $this.find('._button-content').text('Terug naar de beschrijving');
+    } else {
+        $this.find('._button-content').text('Bekijk de details');
+    }
+});
+
+/// ------------------------------------------------------- SHOWING THE MAP
+
+$('._funda-tile-show-location').on('click', function(e) {
+    e.preventDefault();
+    $(this).closest('.funda-tile').find('.funda-tile__top').toggleClass('funda-tile__top--show-map');
 });
